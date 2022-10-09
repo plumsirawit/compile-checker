877883e32577d3078385b9c6d4bcbcd7b6dda2d7
Sirawit <plurm2545@hotmail.com>
2022-10-10 00:20:26 +0200

    feat: handle compilation

--- compile-checker-backend/handler.py
+++ compile-checker-backend/handler.py
@@ -1,5 +1,8 @@
 import json
-
+import os
+from secret import SECRET
+import base64
+from uuid import uuid4
 
 def hello(event, context):
     body = {
@@ -8,3 +11,35 @@ def hello(event, context):
     }
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
+
+def compile(event, context):
+    body = json.loads(event['body'])
+    if body.get('secret', '') != SECRET:
+        return {
+            "statusCode": 403,
+            "body": json.dumps({
+                "message": "Invalid secret",
+                "input": event,
+            })
+        }
+    source = body.get('source', '')
+    file_name = str(uuid4())
+    with open(f'/tmp/{file_name}.cpp', 'w') as f:
+        f.write(source)
+    os.system(f'g++ -std=c++11 -O2 /tmp/{file_name}.cpp -o /tmp/{file_name}')
+    with open(f'/tmp/{file_name}', 'rb') as f:
+        output_file_content = base64.b64encode(f.read()).decode('utf-8')
+    os.remove(f'/tmp/{file_name}.cpp')
+    os.remove(f'/tmp/{file_name}')
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": output_file_content,
+            "input": event,
+        }),
+        "headers": {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        }
+    }
