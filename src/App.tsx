129044f7810faa9f35df7f7e0fca2c481252f3bb
Sirawit <plurm2545@hotmail.com>
2022-10-10 00:56:27 +0200

    feat: frontend connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import logo from "./ITTT_logo_v1_square.png";
+import "./App.css";
+import { saveAs } from "file-saver";
+import { Buffer } from "buffer";
 
 function App() {
+  const [passcode, setPasscode] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Automatic checker.cpp compiler</p>
+        <input
+          type="text"
+          placeholder="passcode"
+          onChange={(e) => setPasscode(e.target.value)}
+        />
+        <br />
+        <input type="file" ref={fileInputRef} />
+        <br />
+        <input
+          type="submit"
+          onClick={async () => {
+            const file = fileInputRef?.current?.files?.[0];
+            if (!file) {
+              alert("File not attached");
+              return;
+            }
+            const text = await file.text();
+            try {
+              const resp = await fetch(
+                "https://3ash30diz0.execute-api.ap-southeast-1.amazonaws.com/dev/compile",
+                {
+                  method: "post",
+                  body: JSON.stringify({
+                    secret: passcode,
+                    source: text,
+                  }),
+                }
+              );
+              if (resp.status !== 200) {
+                alert(`something is wrong (code is ${resp.status}, not 200).`);
+                return;
+              }
+              const respJson = await resp.json();
+              const buffer = Buffer.from(respJson.message, "base64");
+              saveAs(
+                new Blob([buffer], { type: "application/octet-stream" }),
+                "checker"
+              );
+            } catch (e) {
+              alert(e);
+            }
+          }}
+        />
       </header>
     </div>
   );
